216a757632386ef13972c36d0d7ce15a1dae1956
Liriel-Felix <lirielfelixblog@gmail.com>
2024-07-03 19:57:22 -0300

    line

---
+++ build/CMakeFiles/snaze-game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snaze-game.dir/src/game.cpp.o"
+  "CMakeFiles/snaze-game.dir/src/game.cpp.o.d"
+  "CMakeFiles/snaze-game.dir/src/main.cpp.o"
+  "CMakeFiles/snaze-game.dir/src/main.cpp.o.d"
+  "snaze-game"
+  "snaze-game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snaze-game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
